4703c136c75a8f43981de3daff9a39b762ab11b8
Remy Cassagne <remy.engassac@icloud.com>
2025-01-09 09:26:47 +0100

    'done'

--- src/app/api/v1/files/[id]/route.ts
+++ src/app/api/v1/files/[id]/route.ts
@@ -6,25 +6,36 @@ export async function GET(
     request: Request,
     { params }: { params: { id: string } }
 ) {
-    const videoPath = decodeURIComponent(params.id);
+    const { id } = params;
+    const videoPath = decodeURIComponent(id);
     const directoryPath = path.join(process.cwd(), "public", path.dirname(videoPath));
 
-    try {
-        const files = await fs.promises.readdir(directoryPath);
-        const jsonFile = files.find(file => path.extname(file).toLowerCase() === '.json');
+    return fs.promises.readdir(directoryPath)
+        .then(files => {
+            const jsonFile = files.find(file => path.extname(file).toLowerCase() === '.json');
+            
+            if (!jsonFile) {
+                return NextResponse.json({ jsonData: null }, { status: 404 });
+            }
 
-        if (jsonFile) {
             const jsonPath = path.join(directoryPath, jsonFile);
-            const jsonData = await fs.promises.readFile(jsonPath, 'utf8');
-            return NextResponse.json({ jsonData: JSON.parse(jsonData) }, { status: 200 });
-        }
-
-        return NextResponse.json({ jsonData: null }, { status: 404 });
-    } catch (error) {
-        console.error("Error reading JSON file:", error);
-        return NextResponse.json(
-            { error: "Error reading JSON file" },
-            { status: 500 }
-        );
-    }
+            return fs.promises.readFile(jsonPath, 'utf8')
+                .then(jsonData => {
+                    return NextResponse.json({ jsonData: JSON.parse(jsonData) }, { status: 200 });
+                })
+                .catch(error => {
+                    console.error("Error reading JSON file:", error);
+                    return NextResponse.json(
+                        { error: "Error reading JSON file" },
+                        { status: 500 }
+                    );
+                });
+        })
+        .catch(error => {
+            console.error("Error reading directory:", error);
+            return NextResponse.json(
+                { error: "Error reading directory" },
+                { status: 500 }
+            );
+        });
 }
