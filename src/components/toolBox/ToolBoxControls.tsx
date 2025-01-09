f5203ff580b8ec25a9458b52c26ab0bcb3563a16
Remy Cassagne <remy.engassac@icloud.com>
2025-01-09 17:38:38 +0100

    change goto frame icon button

--- src/components/toolBox/ToolBoxControls.tsx
+++ src/components/toolBox/ToolBoxControls.tsx
@@ -1,4 +1,4 @@
-import { faBackward, faBackwardStep, faForward, faForwardStep, faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faBackward, faBackwardStep, faForward, faForwardStep, faMagnifyingGlass, faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useRef } from 'react'
 import ControlBtn from './ControlBtn';
 import { useActiveLayers } from '@/context/layers';
@@ -6,6 +6,7 @@ import { useVideoPlayer } from '@/hooks/useVideoPlayer';
 import { useFrame } from '@/context/frame';
 import { drawElements } from '@/utils/drawing/drawElements';
 import { JSONData } from '@/types/files';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ToolBoxControls({ videoData }: { videoData: JSONData }) {
     const { currentFrame, setCurrentFrame } = useFrame();
@@ -60,7 +61,9 @@ export default function ToolBoxControls({ videoData }: { videoData: JSONData }) 
                 <p className="text-white font-semibold text-base">Frame ID : {currentFrame}</p>
                 <form className="flex gap-2 items-center" onSubmit={handleSubmit}>
                     <input type="number" step={10} className="w-24 h-6 bg-lighterBackground rounded-md p-2 text-center text-white font-semibold text-base outline-none border-none focus:ring-primary focus:ring-1" />
-                    <button className="bg-primary text-white font-semibold text-base rounded-md px-2 active:bg-primary/80 transition-all duration-200" type="submit">Go</button>
+                    <button className="bg-primary text-white font-semibold text-base rounded-md flex items-center justify-center px-2 py-1 active:bg-primary/80 transition-all duration-200" type="submit">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </form>
             </div>
             <div className="flex gap-4 items-center justify-center">
