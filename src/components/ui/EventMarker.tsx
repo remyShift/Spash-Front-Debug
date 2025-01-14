097674e3a8d43323d50d9596b48669eea5b064b4
Remy Cassagne <remy.engassac@icloud.com>
2025-01-14 14:15:09 +0100

    marker button work

--- src/components/ui/EventMarker.tsx
+++ src/components/ui/EventMarker.tsx
@@ -1,8 +1,11 @@
 import { faMapPin } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Event } from '@/types/events';
+import { useFrame } from '@/context/frame';
 
-export default function EventMarker({ event }: { event: Event }) {
+export default function EventMarker({ event, frame }: { event: Event, frame: number }) {
+    const { setCurrentFrame } = useFrame();
+
     const colorMarker = {
         hits: 'text-primary',
         lobs: 'text-green-500',
@@ -13,6 +16,17 @@ export default function EventMarker({ event }: { event: Event }) {
     }
 
     return (
-        <FontAwesomeIcon icon={faMapPin} className={`${colorMarker[event]} text-lg`}/>
+        <button className="relative" onClick={() => {
+            setCurrentFrame(frame).then(() => {
+                const video = document.querySelector('video');
+                if (video) {
+                    const FPS = 25;
+                    const timeInSeconds = frame / FPS;
+                    video.currentTime = timeInSeconds;
+                }
+            });
+        }}>
+            <FontAwesomeIcon icon={faMapPin} className={`${colorMarker[event]} text-lg`}/>
+        </button>
     )
 }
