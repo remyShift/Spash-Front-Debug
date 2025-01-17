6281550d1795cd9be140cd8f6e4afa3825434a38
Remy Cassagne <remy.engassac@icloud.com>
2025-01-17 15:58:52 +0100

    'done'

--- src/components/toolBox/GotoPlayer.tsx
+++ src/components/toolBox/GotoPlayer.tsx
@@ -33,7 +33,7 @@ export default function GotoPlayer({ handleFrameChange, videoData }: {
 
     return (
         <div className="flex gap-4 items-center justify-center">
-            <p className="text-white font-semibold text-base">Aller au Joueur :</p>
+            <p className="text-white font-semibold text-base">Go to Player :</p>
             <form className="flex gap-2 items-center" onSubmit={handleSubmit}>
                 <input type="number" min={0} className="w-24 h-6 bg-lighterBackground rounded-md p-2 text-center text-white font-semibold text-base outline-none border-none focus:ring-primary focus:ring-1" />
                 <button className="bg-primary text-white font-semibold text-base rounded-md flex items-center justify-center px-2 py-1 active:bg-primary/80 transition-all duration-200" type="submit">
