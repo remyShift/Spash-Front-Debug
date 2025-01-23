462c0264035a41969d81418bdc8c80312114d86e
Remy Cassagne <remy.engassac@icloud.com>
2025-01-23 14:51:21 +0100

    'done'

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,13 +1,23 @@
 
 export default function Footer() {
     return (
-        <div className="h-12 flex justify-center items-center gap-1">
-            <p className="text-sm text-gray-300">
-                Made with ❤️ by
+        <div className="h-12 flex flex-col justify-center items-center">
+            <div className="h-12 flex justify-center items-center gap-1">
+                <p className="text-sm text-gray-300">
+                    Made with ❤️ by
             </p>
             <a href="https://www.remyshift.dev" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">
                 remyShift
             </a>
+        </div>
+        <div className="h-12 flex justify-center items-center gap-1">
+            <p className="text-sm text-gray-300">
+                During my internship at 
+            </p>
+                <a href="https://www.mad-dev.fr" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">
+                    Spash
+                </a>
+            </div>
         </div>
     );
 }
