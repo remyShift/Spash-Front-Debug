0dbe52cc0fb6a088d2a5512f2260315035b0b4df
Rémy Cassagne <remy_mac@MacBook-Pro-de-Remy.local>
2025-02-17 13:54:27 +0100

    add frame idx on video

--- src/components/videoPlayer/VideoPlayer.tsx
+++ src/components/videoPlayer/VideoPlayer.tsx
@@ -11,6 +11,7 @@ import { RenderTiming } from '@/types/performance';
 import { usePerformance } from '@/context/performance';
 import HomographyPoints from '@/components/videoPlayer/layers/HomographyPoints';
 import { useSport } from '@/context/sport';
+import FrameIdx from '../ui/FrameIdx';
 
 interface VideoPlayerProps {
     currentVideo: VideoInfo;
@@ -112,6 +113,8 @@ export const VideoPlayer = ({ currentVideo, jsonData, activeLayers, statsData }:
                     videoRef={videoRef as React.RefObject<HTMLVideoElement>} 
                     videoData={jsonData}
                 />
+
+                <FrameIdx frameIdx={currentFrame} />
 
                 {(jsonData.info.cfg.sport === 'padel' && activeLayers.includes('killfeed')) && (
                     <KillFeed 
